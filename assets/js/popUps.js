1e0d9cd0a9fd81596c21393ac06b0174f6e8d29b
arab samir <arab.samir@hotmail.com>
2020-03-04 11:06:04 +0100

    open popup on btn click

--- assets/js/popUps.js
+++ assets/js/popUps.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     var loginPopUpElt = document.getElementById('p_u_login');
+    var beRecipentElt = document.getElementById('be_recipent_bt_id');
+    var beRecipentElt2 = document.getElementById('be_recipent_bt2_id');
     if(loginPopUpElt != null) {
         var loginPopUpCloseElt = document.getElementById('p_u_login_close');
         loginPopUpCloseElt.addEventListener('click', ()=> {
             closePopUp(loginPopUpElt);
         });
+        beRecipentElt.addEventListener('click', ()=> {
+            openPopUp(loginPopUpElt);
+        });
+        beRecipentElt2.addEventListener('click', ()=> {
+            openPopUp(loginPopUpElt);
+        });
     }
+
 });
 
 function closePopUp(elt) {
     elt.style.display = 'none';
+}
+
+function openPopUp(elt) {
+    elt.style.display = '';
 }
